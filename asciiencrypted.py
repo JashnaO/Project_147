829667b99c033abbbed929f2e269e7a6bada4b0e
JashnaO <77934083+JashnaO@users.noreply.github.com>
2021-04-04 09:06:14 -0700

    Add files via upload

---
+++ asciiencrypted.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+#Uncomment the following line if you have MacOS
+#Ask the student to write the following line if the student has MacOS
+
+#from tkmacosx import *
+
+root=Tk()
+root.title("Ascii Encryption")
+
+root.geometry("400x400")
+root.configure(background = 'snow')
+
+enter_word = Entry(root)
+enter_word.place(relx=0.5,rely=0.4,anchor=CENTER)
+
+label = Label(root, text = "Name in Ascii : ", bg="light yellow", fg="black")
+label2 = Label(root, text = "Encrypted name= : ", bg="light yellow", fg="black")
+
+def asciiConverter():
+    input_word = enter_word.get()
+
+    for letter in input_word :
+        label["text"] += str(ord(letter)) + "  "
+        ascii=int(ord(letter))
+        encrypted=ascii-1
+        label2["text"] += str(chr(encrypted))
+        
+btn=Button(root,text="Display the ascii code and encrypted value",command=asciiConverter, bg='gold', fg = 'black')
+btn.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+label.place(relx=0.5,rely=0.6,anchor=CENTER)
+label2.place(relx=0.5,rely=0.7,anchor=CENTER)
+
+root.mainloop()
+
+
